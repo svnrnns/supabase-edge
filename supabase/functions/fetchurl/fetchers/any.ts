5d0ac74dd2320db6f030e9349afbf519a1022db7
svnrnns <haruueel@gmail.com>
2024-08-03 18:36:37 +0200

    feat(fetchurl): any url processing

---
+++ supabase/functions/fetchurl/fetchers/any.ts
@@ -0,0 +1,37 @@
+import * as cheerio from 'npm:cheerio@1.0.0-rc.12';
+import { getMetatag, getHTML, generateDataTemplate } from '../services/data.ts';
+
+export async function handle(url: string) {
+  return await fetchGeneralData(url);
+}
+
+async function fetchGeneralData(url) {
+  const apiResponse = generateDataTemplate(url);
+  const html = await getHTML(url);
+  const $ = cheerio.load(html);
+
+  const title = $('title').text();
+  const description = $('meta[name="description"]').attr('content');
+  const imageUrl = $('meta[property="og:image"]').attr('content');
+  const faviconLink = $('link[rel="icon"], link[rel="shortcut icon"]')
+    .first()
+    .attr('href');
+  const touchIconLink = $('link[rel="apple-touch-icon"]').attr('href');
+
+  const faviconUrl = new URL(faviconLink, url).toString();
+  const touchIconUrl = new URL(touchIconLink, url).toString();
+  const s2FaviconUrl =
+    'https://s2.googleusercontent.com/s2/favicons?domain_url=' + url;
+
+  apiResponse.data = {
+    title,
+    description,
+    image: imageUrl,
+    favicon: faviconUrl,
+    touchicon: touchIconUrl,
+    s2_favicon: s2FaviconUrl,
+  };
+  apiResponse.status = 'COMPLETED';
+
+  return apiResponse;
+}
--- supabase/functions/fetchurl/services/http.ts
+++ supabase/functions/fetchurl/services/http.ts
@@ -1,23 +1,28 @@
 import { track } from '../fetchers/spotify.ts';
 import { profile } from '../fetchers/instagram.ts';
+import { handle } from '../fetchers/any.ts';
 
 import { failedResponse } from './data.ts';
 
 const spotifyURL = 'open.spotify.com';
 const instagramURL = 'instagram.com';
 
+function buildResponse(response: any, url: string): Object {
+  return response === undefined ? failedResponse(url) : response;
+}
+
 export async function process(url: string) {
   let response = undefined;
 
   if (url.includes(spotifyURL) && url.includes('/track/')) {
     response = await track(url);
+    return buildResponse(response, url);
   }
 
   if (url.includes(instagramURL)) {
     response = await profile(url);
+    return buildResponse(response, url);
   }
 
-  //   any.handle(url, res);
-
-  return response === undefined ? failedResponse(url) : response;
+  return buildResponse(handle(url), url);
 }
